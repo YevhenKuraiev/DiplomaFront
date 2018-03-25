df1f538968453f807c75e0696d448e8e90e9dd73
VLADISLAV-PC\Vladislav <vladislav.bardamid@gmail.com>
2018-03-26 02:23:02 +0300

    Working with editors

--- src/app/admin/adminDishes/adminDishes.component.ts
+++ src/app/admin/adminDishes/adminDishes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../../services/http.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Dish } from '../../models/dishModel';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-admin-dishes',
@@ -17,25 +18,41 @@ export class AdminDishesComponent implements OnInit {
   showEditCategory = false;
 
   imageData: any;
-  dishes: Dish;
+  dishes: Dish[] = [];
+
+  pageSize = 10;
+  pageSizeOptions = [5, 10, 25, 100];
+  pageEvent: PageEvent;
+
+  editIndex: number;
 
   constructor(private httpService: HttpService) {
     this.editDishForm = new FormGroup({
       'name': new FormControl('', [
         Validators.required,
-        Validators.pattern('[А-Я][а-яА-Я]+')
+        Validators.pattern('[А-Я][а-яА-Я]{2,}')
       ]),
     });
     this.addDishForm = new FormGroup({
       'name': new FormControl('', [
         Validators.required,
-        Validators.pattern('[А-Я][а-яА-Я]+')
+        Validators.pattern('[А-Я][а-яА-Я]{2,}')
       ]),
     });
   }
 
   ngOnInit(): void {
-    this.httpService.getData('dishes').subscribe((data: Dish) => this.dishes = data);
+    this.httpService.getData('dishes').subscribe((data: Dish[]) => this.dishes = data);
+  }
+
+  getDishes() {
+    if (this.pageEvent) {
+      return this.dishes.slice(
+        (this.pageEvent.pageIndex * this.pageEvent.pageSize),
+        (this.pageEvent.pageIndex * this.pageEvent.pageSize + this.pageEvent.pageSize));
+    } else {
+      return this.dishes.slice(0, this.pageSize);
+    }
   }
 
 }
